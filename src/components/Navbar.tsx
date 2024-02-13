80d61724215b6c1c9ef7c15bd82d76eed6a81023
Emil Alvaro Serrano Danguilan <158530478+emilalvaro2024@users.noreply.github.com>
2024-02-13 08:55:14 +0800

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ function Navbar() {
     <header className="px-2 py-2 sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <nav className="flex justify-between items-center max-w-4xl mx-auto px-5">
         <Link to="/">
-          <h1 className="font-bold text-xl text-primary">YTS Movies</h1>
+          <h1 className="font-bold text-xl text-primary">Momuflix</h1>
         </Link>
         <ul className="flex gap-2 items-center">
           <li>
